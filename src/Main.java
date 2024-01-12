880e799c611d67043be661037799218072fdc963
mnjh01 <119735814+mnjihwan@users.noreply.github.com>
2024-01-12 15:26:44 +0900

    while if

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         RunShootingGame runShootingGame = new RunShootingGame();
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("사격 개시 : 1\n사격 종료 : 0");
 
-        while (true) {
-            int isStart = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-            switch (isStart) {
-                case 0: System.out.println("프로그램 종료");
-                        break;
-                case 1: runShootingGame.run();
-                        System.out.println("사격 재개 : 1\n사격 종료 : 0");
-                        break;
-                default: System.out.println("1 또는 0을 입력해주세요.");
-                        break;
+        int isStart = 0;
+
+        while (true) {
+
+            isStart = scanner.nextInt();
+
+            if (isStart == 0) {
+                System.out.println("프로그램 종료");
+                break;
+            } else if (isStart == 1) {
+                runShootingGame.run();
+            } else {
+                System.out.println("1 또는 0을 입력해주세요.");
             }
+
         }
     }
 }
